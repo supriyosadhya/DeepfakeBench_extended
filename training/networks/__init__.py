796f339a774abe493b6cde12bb6de002ebd79892
supriyosadhya <101490016+supriyosadhya@users.noreply.github.com>
2024-05-06 18:40:22 -0600

    Update __init__.py

--- training/networks/__init__.py
+++ training/networks/__init__.py
@@ -1,5 +1,14 @@
 from utils.registry import BACKBONE
 from .xception import Xception
+from .xception_wv import Xception_wv
+from .xception_wv2 import Xception_wv2
 from .mesonet import Meso4, MesoInception4
 from .resnet34 import ResNet34
+from .vgg19 import Vgg19
+from .vgg19_att import Vgg19_att
+from .vgg19_spatial import Vgg19_spatial
+from .vgg19_wv import Vgg19_wv
+from .vgg19_wv_new import Vgg19_wv_new
+from .vgg19_wv2 import Vgg19_wv2
+from .vgg19_wv2_new import Vgg19_wv2_new
 from .efficientnetb4 import EfficientNetB4
