5c90c196e35acb3567146e3a99da73ddac2325db
supriyosadhya <101490016+supriyosadhya@users.noreply.github.com>
2024-05-06 19:01:06 -0600

    Update __init__.py

--- training/detectors/__init__.py
+++ training/detectors/__init__.py
@@ -3,6 +3,8 @@ from .facexray_detector import FaceXrayDetector
 from .xception_detector import XceptionDetector
 from .efficientnetb4_detector import EfficientDetector
 from .resnet34_detector import ResnetDetector
+from .vgg19wv_detector import VggwvDetector
+from .vgg19_detector import VggDetector
 from .f3net_detector import F3netDetector
 from .meso4_detector import Meso4Detector
 from .meso4Inception_detector import Meso4InceptionDetector
@@ -14,3 +16,4 @@ from .ucf_detector import UCFDetector
 from .recce_detector import RecceDetector
 from .fwa_detector import FWADetector
 from .ffd_detector import FFDDetector
+from .xception_spatial_ffd_detector import XceptionSpatialDetector
